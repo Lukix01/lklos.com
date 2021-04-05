8a7521ab6883584601d193664a2fc85e6e8b5763
Łukasz Kłos <39856157+Lukix01@users.noreply.github.com>
2021-04-05 20:13:33 +0200

    redesigned site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        basic: ['Inconsolata']
+      },
+      textColor: {
+        primary: '#495464'
+      }
+    },
   },
   variants: {
     extend: {},
