cab65892ac3b538f4ffd3c20912ad4bf2aec6770
Łukasz Kłos <39856157+Lukix01@users.noreply.github.com>
2021-04-06 15:52:55 +0200

    first portfolio project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        basic: ['Inconsolata']
-      },
-      textColor: {
-        primary: '#495464'
-      }
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                basic: ["Inconsolata"],
+            },
+            textColor: {
+                primary: "#495464",
+            },
+            spacing: {
+                card: "42rem",
+            },
+            borderRadius: {
+                card: "2rem",
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
