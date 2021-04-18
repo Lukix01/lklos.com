772e7cc26481413a0bdaa87a96a703254df02342
Łukasz Kłos <39856157+Lukix01@users.noreply.github.com>
2021-04-18 12:41:24 +0200

    full responsive homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,45 @@ export default function Home() {
             <Head>
                 <title>lklos.com | Homepage</title>
             </Head>
-            <div className="flex flex-col justify-content items-center">
-                <div className="animate-pulse text-8xl mt-80 text-gray-400 font-bold">
+            <div className="justify-content items-center">
+                <div className="text-center animate-pulse text-6xl md:text-8xl mt-44 md:mt-80 text-gray-400 font-bold">
                     Łukasz Kłos
                 </div>
             </div>
-            <div className="mt-3 text-3xl flex justify-center">
+            <div className="mt-3 text-3xl md:flex  text-center justify-center">
                 <Link href="https://github.com/Lukix01">
                     <a target="blank">
-                        <div className="hover:text-gray-400 cursor-pointer">Github</div>
+                        <div className="mt-4 md:mt-0 hover:text-gray-400 cursor-pointer">
+                            Github
+                        </div>
                     </a>
                 </Link>
                 <Link href="https://twitter.com/LukiXKK">
                     <a target="blank">
-                        <div className="ml-7 hover:text-gray-400 cursor-pointer">Twitter</div>
+                        <div className="md:ml-7 mt-4 md:mt-0 hover:text-gray-400 cursor-pointer">
+                            Twitter
+                        </div>
                     </a>
                 </Link>
                 <Link href="https://discord.gg/pmpszsf7Cf">
                     <a target="blank">
-                        <div className="ml-7 hover:text-gray-400 cursor-pointer">Discord</div>
+                        <div className="md:ml-7 mt-4 md:mt-0 hover:text-gray-400 cursor-pointer">
+                            Discord
+                        </div>
                     </a>
                 </Link>
                 <Link href="mailto:lukaszklosx@gmail.com">
                     <a target="blank">
-                        <div className="ml-7 hover:text-gray-400 cursor-pointer">E-Mail</div>
+                        <div className="md:ml-7 mt-4 md:mt-0 hover:text-gray-400 cursor-pointer">
+                            E-Mail
+                        </div>
                     </a>
                 </Link>
-                <div className="ml-7 text-gray-400">|</div>
+                <div className="hidden md:block ml-7 text-gray-400">|</div>
                 <Link href="/portfolio">
-                    <div className="ml-7 hover:text-gray-400 cursor-pointer">Portfolio</div>
+                    <div className="md:ml-7 mt-4 md:mt-0 hover:text-gray-400 cursor-pointer">
+                        Portfolio
+                    </div>
                 </Link>
             </div>
         </div>
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -2,7 +2,11 @@ import Head from "next/head";
 import Link from "next/link";
 import Card from "../components/Card";
 
+let visits = 0;
+
 const Portfolio = () => {
+    visits = visits + 1;
+    console.log(visits);
     return (
         <div className="text-white font-basic overflow-auto">
             <Head>
@@ -17,16 +21,16 @@ const Portfolio = () => {
                 </div>
             </div>
             <Card
+                name="Cactais"
+                date="16 April 2021"
+                link="https://github.com/Lukix01/Cactais"
+                image="img/cactais.png"
+            />
+            <Card
                 name="Lukix Bot"
                 date="27 March 2021"
                 link="https://discord.com/oauth2/authorize?client_id=806225903713255507&permissions=592896&scope=bot"
                 image="img/lukix-bot.png"
-            />
-            <Card
-                name="Biegnij!"
-                date="3 October 2020"
-                link="https://lukix.itch.io/biegnij"
-                image="img/biegnij.png"
             />
             <Card
                 name="Eat It!"
@@ -35,10 +39,10 @@ const Portfolio = () => {
                 image="img/eat-it.png"
             />
             <Card
-                name="Cactais"
-                date="16 April 2021"
-                link="https://github.com/Lukix01/Cactais"
-                image="img/cactais.png"
+                name="Biegnij!"
+                date="3 October 2020"
+                link="https://lukix.itch.io/biegnij"
+                image="img/biegnij.png"
             />
         </div>
     );
