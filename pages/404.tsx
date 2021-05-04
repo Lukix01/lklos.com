6d4a0a1637ad57792d97c949a6a2a4c668f72089
Łukasz Kłos <39856157+Lukix01@users.noreply.github.com>
2021-05-04 19:30:09 +0200

    new '404' error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,23 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Head from "next/head";
+import Layout from "../components/Layout";
 
-export default function Error404() {
-    const router = useRouter();
-
-    useEffect(() => {
-        router.push("/");
-    });
-
-    return <></>;
+export default function Contact() {
+    return (
+        <>
+            <Layout
+                name="404 Error"
+                icon="my-auto ml-4 fas fa-exclamation-triangle"
+                color="text-red"
+            />
+            <div className="text-white font-basic font-bold">
+                <Head>
+                    <title>lklos.com | 404 Error</title>
+                </Head>
+                <div className="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl">
+                    <i className="fas fa-exclamation-triangle text-9xl animate-pulse text-red"></i>
+                    <div className="mt-8">The page could not be found</div>
+                </div>
+            </div>
+        </>
+    );
 }
