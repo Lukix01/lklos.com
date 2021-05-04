f34d1e8134be31d4abd5d0743e1f60ab203ef9d4
Łukasz Kłos <39856157+Lukix01@users.noreply.github.com>
2021-05-04 17:25:20 +0200

    new homepage hovers

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,38 @@ export default function Home() {
             <Head>
                 <title>lklos.com | Homepage</title>
             </Head>
-            <div className="flex items-center  w-52 h-52 bg-basic rounded-basic">
-                <i className="m-auto fas fa-6x fa-list-alt text-yellow"></i>
-            </div>
-            <div className="flex items-center w-52 h-52 bg-basic rounded-basic">
-                <i className="m-auto fas fa-6x fa-address-card text-green"></i>
-            </div>
-            <div className="flex items-center w-52 h-52 bg-basic rounded-basic">
-                <i className="m-auto fas fa-6x fa-gamepad text-red"></i>
-            </div>
-            <div className="flex items-center w-52 h-52 bg-basic rounded-basic">
-                <i className="m-auto fas fa-6x fa-user-lock text-blue"></i>
-            </div>
+            <Link href="/projects">
+                <div className="group transform duration-500 motion-safe:hover:scale-105 flex items-center w-52 h-52 bg-basic rounded-basic">
+                    <i className="m-auto fas fa-6x fa-list-alt text-yellow transform duration-500 group-hover:opacity-25"></i>
+                    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 text-3xl font-bold transform duration-500 select-none">
+                        Projects
+                    </div>
+                </div>
+            </Link>
+            <Link href="/contact">
+                <div className="group transform duration-500 motion-safe:hover:scale-105 flex items-center w-52 h-52 bg-basic rounded-basic">
+                    <i className="m-auto fas fa-6x fa-envelope text-green transform duration-500 group-hover:opacity-25"></i>
+                    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 text-3xl font-bold transform duration-500 select-none">
+                        Contact
+                    </div>
+                </div>
+            </Link>
+            <Link href="/">
+                <div className="group transform duration-500 motion-safe:hover:scale-105 flex items-center w-52 h-52 bg-basic rounded-basic">
+                    <i className="m-auto fas fa-6x fa-gamepad text-red transform duration-500 group-hover:opacity-25"></i>
+                    <div className="absolute text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 text-3xl font-bold transform duration-500 select-none">
+                        Current Game
+                    </div>
+                </div>
+            </Link>
+            <Link href="/">
+                <div className="group transform duration-500 motion-safe:hover:scale-105 flex items-center w-52 h-52 bg-basic rounded-basic">
+                    <i className="m-auto fas fa-6x fa-address-card text-blue transform duration-500 group-hover:opacity-25"></i>
+                    <div className="absolute text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 text-3xl font-bold transform duration-500 select-none">
+                        About Me
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 }
