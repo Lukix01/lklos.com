2b864e6e7a6161560179acc255d30be12dec5d2e
Lukix01 <e@lklos.com>
2022-03-25 20:07:08 +0100

    new homepage

--- components/home/PageButton.tsx
+++ components/home/PageButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function PageButton({ name, page }: Props) {
     return (
         <Link href={page}>
-            <div className="font-basic text-3xl cursor-pointer">
+            <div className="text-3xl cursor-pointer opacity-50 hover:opacity-70 transition hover:rotate-3 hover:scale-125">
                 <h1>{name}</h1>
             </div>
         </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import PageButton from "../components/home/PageButton";
 
 export default function Home() {
     return (
-        <div className="flex h-screen">
-            <div className="m-auto text-center">
+        <div className="flex h-screen font-basic">
+            <div className="m-auto text-center space-y-3 ">
                 <PageButton page="/projects" name="Portfolio" />
+                <PageButton page="/projects" name="Socials" />
             </div>
         </div>
     );
