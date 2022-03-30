3ad4c4008ffd0a7f253d8be66a916b7bbff1aa46
Lukix01 <e@lklos.com>
2022-03-30 11:21:31 +0200

    homepage button

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,23 +3,34 @@ import Project from "../components/projects/Project";
 import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { projects } from "../projects.json";
+import Link from "next/link";
 
 export default function Projects() {
     return (
         <div className="flex h-screen font-basic">
-            <div className="m-auto divide-y divide-neutral-800">
-                {projects.map((project: any) => {
-                    return (
-                        <div>
-                            <Project
-                                name={project.name}
-                                date={project.date}
-                                links={project.links}
-                                description={project.description}
-                            />
+            <div className="m-auto">
+                <div className="h-96 overflow-auto">
+                    {projects.map((project: any) => {
+                        return (
+                            <div>
+                                <Project
+                                    name={project.name}
+                                    date={project.date}
+                                    links={project.links}
+                                    description={project.description}
+                                    projects={projects}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className="flex">
+                    <Link href="/">
+                        <div className="mx-auto mt-10 text-2xl cursor-pointer opacity-50 hover:opacity-70 transition">
+                            Homepage
                         </div>
-                    );
-                })}
+                    </Link>
+                </div>
             </div>
         </div>
     );
