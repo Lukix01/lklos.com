21adf466b6721b198b2db6fb7ccf3a0251c74516
Lukix01 <e@lklos.com>
2023-03-08 15:19:58 +0100

    three.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
     mode: "jit",
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                basic: ["Caveat"],
-            },
-        },
+        extend: {},
     },
     variants: {
         extend: {},
