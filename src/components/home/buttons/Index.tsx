26baf2bc5785ff5ff2c05b98a6300ab8326eabf4
Lukix01 <e@lklos.com>
2023-11-06 17:23:05 +0100

    fixed href

--- src/components/home/buttons/Index.tsx
+++ src/components/home/buttons/Index.tsx
@@ -3,7 +3,7 @@ import Button from './Button';
 export default function Buttons(): JSX.Element {
   return (
     <div className='mt-4 space-y-2'>
-      <Button name="Projects" href="/work"/>
+      <Button name="Projects" href="/projects"/>
       <Button name="Contact" href="/contact"/>
     </div>
   );
