936878110fd59c56c2e454cdd1cd4d245e0bd537
Lukix01 <e@lklos.com>
2023-11-07 13:47:33 +0100

    better looking code

--- src/components/home/buttons/Button.tsx
+++ src/components/home/buttons/Button.tsx
@@ -11,7 +11,11 @@ export default function Button({ name, href }: Props): JSX.Element {
   const router: NextRouter = useRouter();
 
   return (
-    <motion.div whileHover={{ x: 5 }} className="flex cursor-pointer group" onClick={(): Promise<boolean> => router.push(href)}>
+    <motion.div
+      whileHover={{ x: 5 }}
+      className="flex cursor-pointer group"
+      onClick={(): Promise<boolean> => router.push(href)}
+    >
       <ChevronDoubleRightIcon className="w-6 mr-2 text-emerald-900" />
       <div className="text-xl text-opacity-70 group-hover:text-opacity-100 transition text-white my-auto">
         {name}
