42eca3fc2a9526c3b3a21860f0ab291af8e44c01
Lukix01 <e@lklos.com>
2023-11-09 15:39:55 +0100

    fixed project width

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -34,7 +34,7 @@ export default function Project({ name, year, link, description }: Props): JSX.E
       onClick={(): Promise<boolean> => router.push(link)}
     >
       <ChevronDoubleRightIcon className='w-6 mr-2 text-emerald-900'/>
-      <div className="bg-black bg-opacity-20 p-4 rounded-lg">
+      <div className="bg-black w-full bg-opacity-20 p-4 rounded-lg">
         <div className="flex">
           <div className="text-xl text-opacity-70 group-hover:text-opacity-100 transition text-white">{name}</div>
           <div className="my-auto ml-2 text-sm text-emerald-800 group-hover:text-emerald-700 transition">[ {year} ]</div>
